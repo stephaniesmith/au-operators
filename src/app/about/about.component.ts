3b1e720733107b20139fc75ab29c81303424ae6a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-08-13 08:12:26 -0700

    concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, timer, fromEvent, Observable, noop } from 'rxjs';
+import { interval, timer, fromEvent, Observable, noop, of, concat } from 'rxjs';
 import { subscribeOn, map } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -13,6 +13,12 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(5, 6);
+    const source3$ = of(7, 8, 9);
 
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log);
   }
 }
